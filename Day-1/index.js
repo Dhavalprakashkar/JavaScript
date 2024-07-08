7d08e830bb5f29374d971c74f6b9153e118bf508
Dhavalprakashkar <62957627+Dhavalprakashkar@users.noreply.github.com>
2024-07-09 03:03:34 +0530

    day1 -updated

---
+++ Day-1/index.js
@@ -0,0 +1,40 @@
+console.log(typeof("awdiz"))
+
+console.log(typeof('awdiz'))
+
+console.log(typeof(`awdiz`))
+
+
+console.log(typeof(2))
+
+console.log(typeof("2"))
+
+
+console.log(typeof(true))
+console.log(typeof(false))
+
+var username = "pqr";
+var userage = 10;
+var useremail = "xyz@gmail.com";
+var userpassword = "xyz@123";
+var user;
+
+console.log(username, userage, useremail, userpassword);
+console.log(user , typeof(user));
+
+var number1 = 23456;
+var number2 = 98765;
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 % number2);
+
+var number = 10; // declare and assign
+console.log(number);
+number = 12; // re-assign
+console.log(number);
+var number = 14 ; // re declare
+console.log(number);
+
+
