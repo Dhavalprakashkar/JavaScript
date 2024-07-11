17cdc232c6600f5e554c7e67629fe694b76eb060
Dhavalprakashkar <62957627+Dhavalprakashkar@users.noreply.github.com>
2024-07-11 08:31:59 +0530

    day2 updated

---
+++ Day-2/index.js
@@ -0,0 +1,38 @@
+number = 21;
+console.log(number);
+var number;
+
+
+console.log(10 < 20);
+console.log(10 > 20);
+console.log(10 >= 20);
+console.log(10 <= 20);
+console.log(10 <= 10);
+
+console.log(2 == "2");
+
+console.log(2 === "2");
+
+
+if (false) {
+  console.log("if block");
+} else {
+  console.log("else block");
+}
+
+
+if (10 < 20) {
+  console.log("inside if");
+} else {
+  console.log("inside else");
+}
+
+
+var number1 = 500;
+var number2 = 120;
+
+if (number1 > number2) {
+  console.log("number1 is greater than number2");
+} else {
+  console.log("number1 is lesser than number2");
+}
