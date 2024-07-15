de065ba43493e6c637eee3a40cea89867d941525
Dhavalprakashkar <62957627+Dhavalprakashkar@users.noreply.github.com>
2024-07-16 01:15:24 +0530

    day5 updated

---
+++ Day-5/index.js
@@ -0,0 +1,12 @@
+console.log("Print Number from 20-40:")
+for( var i=20; i<=40; i++){
+    console.log(i)
+}
+console.log("Print Number from 1-100 only odd:")
+for( var i=1; i<=100; i+=2){
+    console.log(i)
+}
+console.log("Print Number from 50-150 in sequence of 10:")
+for( var i=50; i<=150; i+=10){
+    console.log(i)
+}
